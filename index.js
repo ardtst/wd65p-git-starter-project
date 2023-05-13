a34cdb0852ef8de72c7bdfb099eadd3a53891130
JovenIson <dgreytazter@gmail.com>
2023-05-13 17:40:33 +0800

    Adding Name

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const names = ['Juan Dela Cruz', 'John Eric Yuzon','Aveunalliv Dasalla', 'John Angelo David', 'Joven Motul Ison']
+const names = [
+  "Juan Dela Cruz",
+  "John Eric Yuzon",
+  "Aveunalliv Dasalla",
+  "John Angelo David",
+  "Jessica Culcul",
+  "Ian Noble",
+  "Jomell Hope Dela Pena",
+  "Jerome M. Legaspi",
+  "Caezzy Jhonn Bongcayao",
+  "Joven Motul Ison"
+];
 
-for(let i = 0; i < names.length; i++) {
-    console.log(names[i])
+for (let i = 0; i < names.length; i++) {
+  console.log(names[i]);
 }
