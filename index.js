850ef53b40a19275f0effec3a1a8336d8090259b
yanpox2 <yanpox2@gmail.com>
2023-05-13 14:08:07 +0800

    edited by Ian noble

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const names = ['Juan Dela Cruz', 'John Eric Yuzon','Aveunalliv Dasalla', 'John Angelo David', 'Jessica Culcul']
+const names = ['Juan Dela Cruz', 'John Eric Yuzon','Aveunalliv Dasalla', 'John Angelo David', 'Jessica Culcul', 'Ian Noble']
 
 for(let i = 0; i < names.length; i++) {
     console.log(names[i])
