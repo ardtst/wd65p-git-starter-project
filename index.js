64a50d3d8a8f07d7b16ef918822f385eb5f61d49
David <forpersonalonly191@gmail.com>
2023-05-13 12:24:43 +0800

    final commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const names = ['Juan Dela Cruz', 'John Eric Yuzon','Aveunalliv Dasalla']
+const names = ['Juan Dela Cruz', 'John Eric Yuzon','Aveunalliv Dasalla', 'John Angelo David']
 
 for(let i = 0; i < names.length; i++) {
     console.log(names[i])
