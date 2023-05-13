a2f422549b45409e7cdb4cac7506fd2248b2904e
Jessica Culcul <jessicamculcul@gmail.com>
2023-05-13 13:26:13 +0800

    Add name in array

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-const names = ['Juan Dela Cruz', 'John Eric Yuzon','Aveunalliv Dasalla', 'John Angelo David']
-=======
-const names = ['Juan Dela Cruz', 'Joven Motul Ison']
->>>>>>> feature/add-joven-name
+const names = ['Juan Dela Cruz', 'John Eric Yuzon','Aveunalliv Dasalla', 'John Angelo David', 'Jessica Culcul']
 
 for(let i = 0; i < names.length; i++) {
     console.log(names[i])
