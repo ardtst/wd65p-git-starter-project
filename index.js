ba4281edfa9aa7a5578634f5b4d6cc6636dc5b74
Raja Jeff Edward Pariñas Marimuthu <133474900+Jeff030197@users.noreply.github.com>
2023-05-14 14:27:52 +0800

    Index.js

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const names = [
-  "Juan Dela Cruz",
-  "John Eric Yuzon",
-  "Aveunalliv Dasalla",
-  "John Angelo David",
-  "Jessica Culcul",
-  "Ian Noble",
-  "Jomell Hope Dela Pena",
-  "Caezzy Jhonn Bongcayao",
-  "Joven Motul Ison",
-  "Jerome M. Legaspi",
-  "Raja Jeff Edward Marimuthu",
-  "Jerry Joy Ismael" ,
-];
-
-for (let i = 0; i < names.length; i++) {
-  console.log(names[i]);
-}
+    "Juan Dela Cruz",
+    "John Eric Yuzon",
+    "Aveunalliv Dasalla",
+    "John Angelo David",
+    "Jessica Culcul",
+    "Ian Noble",
+    "Jomell Hope Dela Pena",
+    "Caezzy Jhonn Bongcayao",
+    "Joven Motul Ison",
+    "Jerome M. Legaspi",
+    "Raja Jeff Edward Marimuthu",
+    "Jerry Joy Ismael" ,
+  ];
+  
+  for (let i = 0; i < names.length; i++) 
+    console.log(names[i]);
